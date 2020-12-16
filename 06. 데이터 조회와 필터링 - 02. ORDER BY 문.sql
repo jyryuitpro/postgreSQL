8f311fd778393a2c6bbdc1ca98e2f8fbb21ce813
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-12-16 23:34:22 +0900

    06. 데이터 조회와 필터링 - 02. ORDER BY 문

---
+++ 06. 데이터 조회와 필터링 - 02. ORDER BY 문.sql
@@ -0,0 +1,5 @@
+select first_name, last_name from customer order by first_name asc;
+
+select first_name, last_name from customer order by first_name desc;
+
+select first_name, last_name from customer order by first_name asc, last_name desc;
