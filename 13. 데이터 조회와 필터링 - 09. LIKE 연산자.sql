36f67a834191bf46a07716de0356e585b8d3deac
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-01-03 15:36:35 +0900

    13. 데이터 조회와 필터링 - 09. LIKE 연산자

---
+++ 13. 데이터 조회와 필터링 - 09. LIKE 연산자.sql
@@ -0,0 +1,7 @@
+select first_name, last_name from customer where first_name like 'Jen%';
+
+select first_name, last_name from customer where first_name like '%er%';
+
+select first_name, last_name from customer where first_name like '_her%';
+
+select first_name, last_name from customer where first_name not like 'Jen%';
